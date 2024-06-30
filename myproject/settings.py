e5a273cdd9458806532dfbf9f39fce008a23ad75
Imole007 <Xammyp007.com@gmail.com>
2024-06-30 10:48:27 +0100

    done

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,7 @@ EMAIL_USE_SSL = False
 EMAIL_USE_TLS =True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-v342.onrender.com']
 
 
 # Application definition
